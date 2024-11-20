82a47c5cb9c9fdca70029a3ea89f92c97771b8d3
Łukasz Seremak <lukasz.dominik.seremak@gmail.com>
2024-11-20 14:20:39 +0100

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.send('Hello world after manual changes!');
 });
 
 app.listen(8080, function () {
